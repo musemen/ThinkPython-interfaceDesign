287e65aa2f2f4cd9e605a20740a4a445b223023a
Musa Mohannad <musa.mohannad1@gmail.com>
2018-05-01 00:03:41 -0600

    Delete 4.02.py

--- 4.02.py
+++
@@ -1,6 +0,0 @@
-# Exercise 4.2 Write an appropriately general set of functions that can draw
-# flowers like this:
-# [pictures]
-# You can download a solution from thinkpython.com/code/flower.py.
-
-# Current Staus: Incomplete
